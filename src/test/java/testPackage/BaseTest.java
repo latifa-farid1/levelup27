1e45c068f51261406a5f49efce93e32094c1f6b3
latifa <latefamohamed137@gmail.com>
2024-08-18 10:59:07 +0300

    Initial commit

--- src/test/java/testPackage/BaseTest.java
+++ src/test/java/testPackage/BaseTest.java
@@ -1,6 +1,8 @@
 package testPackage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -14,7 +16,24 @@ public class BaseTest {
         driver.get("https://www.google.com/");
        String URLName= driver.getTitle();
         Assert.assertEquals(URLName,"Google");
-
         driver.quit();
+
+
     }
+ @Test
+    public void viewLog(){
+     WebDriver driver;
+     driver= new ChromeDriver();
+     driver.get("https://www.google.com/");
+     WebElement logo = driver.findElement(By.className("lnXdpd"));
+         Assert.assertTrue(logo.isDisplayed());
+         driver.quit();
+ }
+ public void search1(){
+     WebDriver driver= new ChromeDriver();
+     driver.get("https://www.google.com/");
+
+
+ }
+
 }
